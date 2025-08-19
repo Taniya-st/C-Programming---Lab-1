b8a706e0418797f4250fcc51fc20b93139d8eabd
Taniya-st <taniyasamyani218@gmail.com>
2025-08-19 17:37:59 +0530

    Add files via upload

---
+++ Untitled3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+int i, j, c;
+printf("Enter two number");
+scanf("%d %d\n",&i, &j);
+c = i * j;
+printf("Enter the multiplication of two numbers = %d\n", c);
+
+
+return 0;
+}
