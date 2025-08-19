5dbd228105c95dc5bd5a513cc354348ee2d9366e
Taniya-st <taniyasamyani218@gmail.com>
2025-08-19 18:00:51 +0530

    Add files via upload

---
+++ Untitled4.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+ {
+int i, j, c, b , a , d;
+printf("Enter two number: ");
+scanf("%d %d",&i, &j);
+c = i + j;
+b = i - j;
+a = i / j;
+d = i * j;
+printf(" The sum of two numbers = %d\n", c);
+printf(" The difference of two numbers = %d\n", b);
+printf(" The division of two numbers = %d\n", a);
+printf(" The multiplication of two numbers = %d\n", d);
+
+
+return 0;
+
+}
